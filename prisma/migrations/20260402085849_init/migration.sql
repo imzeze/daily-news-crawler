524ea4f76b2c14557ca5458c4b83ee723cbf390c
imzeze <jihyekim019@gmail.com>
2026-04-02 18:01:21 +0900

    migration 파일 추가

---
+++ prisma/migrations/20260402085849_init/migration.sql
@@ -0,0 +1,47 @@
+-- CreateTable
+CREATE TABLE "Keyword" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "value" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "enabled" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Scrap" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "publishedAt" DATETIME NOT NULL,
+    "keyword" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "MailRecipient" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "SentimentKeyword" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "value" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Keyword_value_key" ON "Keyword"("value");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Scrap_keyword_url_key" ON "Scrap"("keyword", "url");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MailRecipient_email_key" ON "MailRecipient"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SentimentKeyword_type_value_key" ON "SentimentKeyword"("type", "value");
